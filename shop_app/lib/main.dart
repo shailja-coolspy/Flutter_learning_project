3c1e251bdb07ce6bff16caf9c114d50fbc2e56ef
Shailja_A <agarwalshailja1@gmail.com>
2022-09-28 23:37:22 +0530

    added shop app product item

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import './screens/products_overview_screen.dart';
 void main() {
   runApp(MyApp());
 }
@@ -10,26 +10,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MyShop',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(   
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
       ),
-      home: MyHomePage(),
+      home: ProductsOverviewScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+// class MyHomePage extends StatelessWidget {
  
-  @override
-  Widget build(BuildContext context) {
+//   @override
+//   Widget build(BuildContext context) {
     
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShop'),
-      ),
-      body:Center(
-        child:Text('lets build shop app'),
-      ),
-    );
-  }
-}
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('MyShop'),
+//       ),
+//       body:Center(
+//         child:Text('lets build shop app'),
+//       ),
+//     );
+//   }
+// }
